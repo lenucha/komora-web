6f6859695f22ce52846acb41ed5b19e037ce7c1f
lenucha <jechova99@seznam.cz>
2023-08-26 13:11:02 +0200

    Udělala jsem nadpis

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav></nav>
+
+<h1>Česká komora tlumočníků znakového jazyka</h1>
+<p>Odkaz na <a href="https://kit.svelte.dev">divnou stránku</a>.</p>
