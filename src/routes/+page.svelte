819ef07e43436d0b5b35ee1abe27600333b374c7
lenucha <jechova99@seznam.cz>
2023-08-26 14:16:02 +0200

    Tvorba úvodní stránky

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,40 @@
-<nav></nav>
+<script lang="ts">
+	import '@fontsource/inter';
+	import '@fontsource/playfair-display/400.css';
+	import '@fontsource/playfair-display/700.css';
+	import './style.css';
+</script>
+
+<nav>
+	<ul>
+		<li>Úvodní stránka</li>
+		<li>O Komoře</li>
+		<li>Kurzy</li>
+		<li>Členství</li>
+		<li>O tlumočení</li>
+		<li>Kontakty</li>
+		<li>Blog</li>
+	</ul>
+</nav>
 
 <h1>Česká komora tlumočníků znakového jazyka</h1>
-<p>Odkaz na <a href="https://kit.svelte.dev">divnou stránku</a>.</p>
+
+<div class="uvodni-slovo">
+	<div class="video" />
+	<div class="text">
+		<p>
+			Česká komora tlumočníků znakového jazyka (Komora) je
+			nezisková organizace. Je pro tlumočníky, kteří se
+			chtějí vzdělávat, mít přehled o aktuálním dění a mít
+			možnost ovlivňovat rozvoj tlumočnické profese.
+		</p>
+		<p>
+			Komora sdružuje všechny tlumočníky bez ohledu na stav
+			jejich sluchu. Členy se mohou stát profesionálové,
+			kteří tlumočí neslyšícím, nedoslýchavým a ohluchlým
+			lidem, studenti tlumočení a všichni, kteří chtějí
+			Komoru podpořit.
+		</p>
+		<p>Komoru dělají tlumočníci pro tlumočníky.</p>
+	</div>
+</div>
